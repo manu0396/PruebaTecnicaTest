1fc2d01b46d044255105a5b43af9795b46135571
manu0396 <manulucas0396@gmail.com>
2024-12-15 19:48:37 +0100

    Fix endpoint

--- app/src/main/java/com/example/pruebatecnicatest/data/remote/ApiService.kt
+++ app/src/main/java/com/example/pruebatecnicatest/data/remote/ApiService.kt
@@ -2,8 +2,10 @@ package com.example.pruebatecnicatest.data.remote
 
 import com.example.pruebatecnicatest.data.remote.models.PostDTO
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface ApiService {
-    @GET("post")
+    @GET("posts")
     suspend fun fetchTransactions(): List<PostDTO>
 }
